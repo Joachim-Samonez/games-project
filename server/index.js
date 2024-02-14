3acb4bb54c9934346c8cf08b94b9baa0b402b948
joachim-samonez <joachimsamonez@gmail.com>
2024-02-14 22:48:09 +0800

    Initialized backend using MongoDB, Mongoose and Dotenv

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+});
