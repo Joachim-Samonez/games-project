dc95f63fa7fc6053a2a3ec12e02eae420df42549
joachim-samonez <joachimsamonez@gmail.com>
2024-02-15 23:31:59 +0800

    Added user model and controller to allow saving to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import AuthRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,24 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Listening to port 3000");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/server/auth", AuthRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
