f006784473379e0f0abd16e90161eeaa51a5d058
joachim-samonez <joachimsamonez@gmail.com>
2024-03-08 01:46:43 +0800

    Preparation for deployment

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import reviewRouter from "./routes/review.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,6 +37,12 @@ app.get("/test", (req, res) => {
 app.use("/server/auth", authRouter);
 app.use("/server/user", userRouter);
 app.use("/server/review", reviewRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
